b6fcbcaeb94d5e2f7ca91f68a2c1c438b17d971f
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2025-03-29 12:09:28 +0300

    Added header and footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,4 +1,18 @@
 <template>
-    footer
-</template>
-<script setup lang="ts"></script>
+    <footer class="footer px-10 py-7 bg-black text-white mt-7">
+      <div class="flex justify-between">
+        <div class="footer__title text-3xl font-bold">
+          Brand Name
+        </div>
+        <div class="flex flex-col mr-21 text-sm">
+          <p>Разработчик</p>
+          <a href="https://kazan.hh.ru/resume/fadbaa4bff064056290039ed1f62786166776e?hhtmFrom=resume_list" target="_blank" class="underline">
+            Борханов Фаниль
+          </a>
+        </div>
+      </div>
+    </footer>
+  </template>
+  
+  <script setup lang="ts"></script>
+  
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,4 +1,21 @@
 <template>
-    header
-</template>
-<script setup lang="ts"></script>
+    <header class="shadow-xl px-10 py-7">
+      <div class="flex justify-between">
+        <div class="text-3xl font-bold">
+          Brand Name
+        </div>
+        <div class="flex items-center cursor-pointer">
+          <Icon
+            name="material-symbols:shopping-basket-outline"
+            class="mr-4 w-[32px] h-[32px]"
+          />
+          <div class="text-sm mr-21">
+            12 товаров
+          </div>
+        </div>
+      </div>
+    </header>
+  </template>
+  
+  <script setup lang="ts"></script>
+  
