7543dabe4073e6344cdf186061d9cb4e2df7e7d3
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2025-03-29 12:11:23 +0300

    Added home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,15 @@
 <template>
-    home
-</template>
-<script setup lang="ts"></script>
+    <div class="flex-1">
+      <div class="flex items-center justify-center min-h-screen">
+        <NuxtLink
+          to="/catalog"
+          class="text-3xl font-bold text-pink-600 underline"
+        >
+          Перейти в раздел каталога
+        </NuxtLink>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts"></script>
+  
