34a34b91c7e644f1999a63f52f63bc5a7121608e
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2025-03-29 12:36:06 +0300

    Added load products from store

--- pages/catalog.vue
+++ pages/catalog.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="flex-1">
-        catalog
+    <div class="flex-1 px-10">
+      <div class="text-3xl font-bold mt-8 mb-6">
+        Тестовая задача
+      </div>
     </div>
-</template>
-<script setup lang="ts"></script>
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue'
+  import { useApplicationStore } from '@/store/application'
+  
+  const store = useApplicationStore()
+  onMounted(() => {
+    store.initStore()
+  })
+  </script>
+  
--- store/application.ts
+++ store/application.ts
@@ -1,12 +1,46 @@
 import { defineStore } from 'pinia'
-
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+import { apiPath } from '~/utils/api'
+import type { Product } from '@/types'
 /**
  * Хранилище приложения для управления глобальными состояниями.
  */
 export const useApplicationStore = defineStore('application', () => {
-  const welcome = 'Привет!'
+  const products: Ref<Product[]> = ref([])
+  const lastUpdated: Ref<number | null> = ref(null)
+  const cacheTime = 5 * 60 * 1000 // 5 минут
 
-  return {
-    welcome,
+  const loadProducts = async () => {
+    const now = Date.now()
+
+    if (lastUpdated.value && now - lastUpdated.value < cacheTime) {
+      console.log('Используем кешированные данные')
+      return
+    }
+    console.log('Загружаем новые')
+    try {
+      products.value = await $fetch<Product[]>(apiPath.catalog)
+      lastUpdated.value = now
+      // Сохраняем данные в localStorage
+      localStorage.setItem('products', JSON.stringify(products.value))
+      localStorage.setItem('lastUpdated', String(lastUpdated.value))
+    }
+    catch (error) {
+      console.error(error)
+    }
   }
+
+  const initStore = () => {
+    const cachedProducts = localStorage.getItem('products')
+    const cachedTime = localStorage.getItem('lastUpdated')
+
+    if (cachedProducts && cachedTime) {
+      products.value = JSON.parse(cachedProducts)
+      lastUpdated.value = Number(cachedTime)
+    }
+    loadProducts()
+  }
+
+  return { products, loadProducts, initStore }
 })
